9d0c20a966ac2fca90cfb7b6f6177d86ec8406c1
rachelkelly <rachelkelly@gmail.com>
2013-05-25 13:56:09 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#from www.pythonware.com/library/tkinter/introduction/hello-tkinter.htm
+#it's all given in 2.7 so some of my conversions might be flawnky
+
+from tkinter import *
+
+root = Tk()
+
+w = Label(root, text="flemp\nfartsdemper\nI'm9")
+w.pack()
+
+root.mainloop()
